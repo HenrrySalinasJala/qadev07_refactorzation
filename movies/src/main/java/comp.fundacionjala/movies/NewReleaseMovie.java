059d2a5f8870bba3fbc3df715796e98662c777b0
Henrry Salinas <es.henrry@gmail.com>
2016-06-13 15:16:00 -0400

    changing the code for calculateRenterPoints method of NewReleaseMovieClass

--- movies/src/main/java/comp.fundacionjala/movies/NewReleaseMovie.java
+++ movies/src/main/java/comp.fundacionjala/movies/NewReleaseMovie.java
@@ -4,7 +4,7 @@ class NewReleaseMovie extends Movie implements IMovie {
 
     public static final double CHARGE_PER_RENTAL_DAY = 3;
     public static final int NEW_RELEASE_MOVIE = 1;
-    public static int FREQUENT_RENTER_POINTS = 1;
+    public static final int FREQUENT_RENTER_POINTS = 1;
     public static final int MINIMUM_RENTAL_DAYS = 1;
 
     public NewReleaseMovie(String title) {
@@ -17,8 +17,9 @@ class NewReleaseMovie extends Movie implements IMovie {
     }
 
     public int calculateRenterFrequentPoints(int daysRented) {
+
         if (daysRented > MINIMUM_RENTAL_DAYS)
-            FREQUENT_RENTER_POINTS++;
+            return FREQUENT_RENTER_POINTS+1;
         return FREQUENT_RENTER_POINTS;
     }
 }
